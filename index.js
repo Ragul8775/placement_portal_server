1c268e9f13e95e83a38bde0573a008ecce4a1f9b
Ragul8775 <Ragul8775@users.devops1245.github.com>
2024-01-23 09:05:47 +0530

    latest Commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const connection = require('./database/connection');
 const app = express();
 const port = 8000;
 
+const salt =10; 
+
 app.use(express.json());
 app.use(cors());
 app.use(reqLogger);
@@ -18,6 +20,29 @@ function reqLogger(req, res, next) {
     next();
   }
 
+  app.post('/register',(req,res)=>{
+    const sql = "INSERT INTO users(`name`,`email`,`password`) VALUE (?)";
+    bcrypt.hash(req.body.password.toString(), salt,(err,hash)=>{
+        if(err){
+            return res.json({Error: "Error in Hashing Password"})
+        }
+        const values =[
+            req.body.name,
+            req.body.email,
+            hash
+        ]
+        console.log([values])
+      connection.query(sql,[values],(err,result)=>{
+        if(err){
+            return res.json({Error:"Unable to Insert the Data"})
+        }
+        return res.json ({Status:"Success"}).status(201)
+        
+      })
+    })
+  
+
+  })
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
     connection.connect((err) => {
